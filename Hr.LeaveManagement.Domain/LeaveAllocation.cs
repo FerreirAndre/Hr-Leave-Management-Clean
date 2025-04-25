a229b0060e5bc83bdbc64628874f5f80a9de52c4
FerreirAndre <adrecruz15@gmail.com>
2025-04-24 22:28:55 -0300

    adding domain project

---
+++ Hr.LeaveManagement.Domain/LeaveAllocation.cs
@@ -0,0 +1,11 @@
+using Hr.LeaveManagement.Domain.Common;
+
+namespace Hr.LeaveManagement.Domain;
+
+public class LeaveAllocation : BaseEntity
+{
+   public int NumberOfDays { get; set; }
+   public LeaveType? LeaveType { get; set; }
+   public int LeaveTypeId { get; set; }
+   public int Period { get; set; }
+}
---
+++ Hr.LeaveManagement.Domain/LeaveType.cs
@@ -0,0 +1,9 @@
+﻿using Hr.LeaveManagement.Domain.Common;
+
+namespace Hr.LeaveManagement.Domain;
+
+public class LeaveType : BaseEntity
+{
+    public string Name { get; set; } = String.Empty;
+    public int DefaultDays { get; set; }
+}
