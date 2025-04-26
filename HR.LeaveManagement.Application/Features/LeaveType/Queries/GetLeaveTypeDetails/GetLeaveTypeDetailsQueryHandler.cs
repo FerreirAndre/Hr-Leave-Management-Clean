e51bfe1324f99a3ad5b991f97e2094ac036e2457
FerreirAndre <adrecruz15@gmail.com>
2025-04-25 21:25:20 -0300

    adding leavetypedetails to profile

---
+++ HR.LeaveManagement.Application/Features/LeaveType/Queries/GetLeaveTypeDetails/GetLeaveTypeDetailsQueryHandler.cs
@@ -0,0 +1,24 @@
+using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveType.Queries.GetLeaveTypeDetails;
+
+public class GetLeaveTypeDetailsQueryHandler : IRequestHandler<GetLeaveTypeDetailsQuery, LeaveTypeDetailsDto>
+{
+    private readonly ILeaveTypeRepository _leaveTypeRepository;
+    private readonly IMapper _mapper;
+
+    public GetLeaveTypeDetailsQueryHandler(ILeaveTypeRepository leaveTypeRepository,IMapper mapper)
+    {
+        this._leaveTypeRepository = leaveTypeRepository;
+        this._mapper = mapper;
+    }
+
+    public async Task<LeaveTypeDetailsDto> Handle(GetLeaveTypeDetailsQuery request, CancellationToken cancellationToken)
+    {
+        var leaveTypeDetails = await _leaveTypeRepository.GetByIdAsync(request.Id);
+        var data = _mapper.Map<LeaveTypeDetailsDto>(leaveTypeDetails);
+        return data;
+    }
+}
