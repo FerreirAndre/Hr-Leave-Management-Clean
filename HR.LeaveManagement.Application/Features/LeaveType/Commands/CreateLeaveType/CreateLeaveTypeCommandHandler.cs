c25d0c23f386674b9b4c771a019cd2a3e95cdea4
FerreirAndre <adrecruz15@gmail.com>
2025-04-28 16:59:26 -0300

    fixing mapper, adding create, delete and update commands and handlers

---
+++ HR.LeaveManagement.Application/Features/LeaveType/Commands/CreateLeaveType/CreateLeaveTypeCommandHandler.cs
@@ -0,0 +1,24 @@
+using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Application.Features.LeaveType.Queries.GetAllLeaveTypes;
+using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveType.Commands.CreateLeaveType;
+
+public class CreateLeaveTypeCommandHandler : IRequestHandler<CreateLeaveTypeCommand, int>
+{
+    private readonly IMapper _mapper;
+    private readonly ILeaveTypeRepository _leaveTypeRepository;
+
+    public CreateLeaveTypeCommandHandler(IMapper mapper, ILeaveTypeRepository leaveTypeRepository)
+    {
+        this._mapper = mapper;
+        this._leaveTypeRepository = leaveTypeRepository;
+    }
+    public async Task<int> Handle(CreateLeaveTypeCommand request, CancellationToken cancellationToken)
+    {
+        var leaveTypeToCreate = _mapper.Map<Domain.LeaveType>(request);
+        await _leaveTypeRepository.CreateAsync(leaveTypeToCreate);
+        return leaveTypeToCreate.Id;
+    }
+}
---
+++ HR.LeaveManagement.Application/Features/LeaveType/Commands/DeleteLeaveType/DeleteLeaveTypeCommand.cs
@@ -0,0 +1,5 @@
+using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveType.Commands.DeleteLeaveType;
+
+public record DeleteLeaveTypeCommand(int Id) : IRequest<Unit>;
